3f5ce86424532140ab44519fdf4b0675a310ad03
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2020-12-25 23:27:23 -0800

    Add AWS variables for provider block

--- variables.tf
+++ variables.tf
@@ -4,6 +4,17 @@ variable "aws_region" {
   description = "aws region to use"
 }
 
+variable "access_key" {
+  type        = string
+  description = "AWS Access key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "AWS secret key"
+  
+}
+
 variable "edge_ip" {
   type        = string
   description = "IP address of edge device for customer gateway"
