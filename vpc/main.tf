c6076ad4f53a945c409cf2c3e5a6eb61166ef2fb
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-02-04 00:23:17 -0800

    comment out nat gw route

--- vpc/main.tf
+++ vpc/main.tf
@@ -246,13 +246,13 @@ resource "aws_network_acl" "prod" {
 
 # Gateways
 
-#resource "aws_internet_gateway" "gw" {
-#  vpc_id = aws_vpc.main.id
-#
-#  tags = {
-#    Name = "${var.vpc_name}-internet-gateway"
-#  }
-#}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-internet-gateway"
+  }
+}
 
 resource "aws_eip" "dev-nat" {
   vpc = true
@@ -285,11 +285,11 @@ resource "aws_route_table" "dev" {
   }
 }
 
-#resource "aws_route" "dev_igw" {
-#  route_table_id            = aws_route_table.dev.id
-#  destination_cidr_block    = "0.0.0.0/0"
-#  gateway_id = aws_internet_gateway.gw.id
-#}
+resource "aws_route" "dev_igw" {
+  route_table_id            = aws_route_table.dev.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+}
 
 resource "aws_route_table" "staging" {
   vpc_id = aws_vpc.main.id
@@ -307,11 +307,11 @@ resource "aws_route_table" "prod" {
   }
 }
 
-resource "aws_route" "dev_nat" {
-  route_table_id            = aws_route_table.dev.id
-  destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.gw.id
-}
+#resource "aws_route" "dev_nat" {
+#  route_table_id            = aws_route_table.dev.id
+#  destination_cidr_block    = "0.0.0.0/0"
+#  nat_gateway_id = aws_nat_gateway.gw.id
+#}
 
 resource "aws_route_table_association" "dev_routes" {
   count = var.subnet_count
