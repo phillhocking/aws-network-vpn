240e77bbde6d020f5e53cc2d143d5cc3b5f23704
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-02-04 00:11:58 -0800

    fix typo

--- vpc/main.tf
+++ vpc/main.tf
@@ -286,7 +286,7 @@ resource "aws_route_table" "dev" {
 }
 
 resource "aws_route" "dev_igw" {
-  route_table_id            = aws_route_table.main.id
+  route_table_id            = aws_route_table.dev.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
 }
