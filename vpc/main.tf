5c760b9626ba5214e705e741c3b0a86ca179091d
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-02-04 00:08:30 -0800

    comment duplicate igw/nat route declarations

--- vpc/main.tf
+++ vpc/main.tf
@@ -277,16 +277,16 @@ resource "aws_nat_gateway" "gw" {
 
 # Route Tables
 
-resource "aws_route_table" "dev" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "dev-route-table"
-  }
-}
+#resource "aws_route_table" "dev" {
+#  vpc_id = aws_vpc.main.id
+#
+#  tags = {
+#    Name = "dev-route-table"
+#  }
+#}
 
 resource "aws_route" "dev_igw" {
-  route_table_id            = aws_route_table.dev.id
+  route_table_id            = aws_route_table.main.id
   destination_cidr_block    = "0.0.0.0/0"
   gateway_id = aws_internet_gateway.gw.id
 }
