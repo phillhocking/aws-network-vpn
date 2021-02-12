862d7ddb46defd89e779d66137ed5ce31ff77a9b
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-02-12 01:13:24 -0800

    simplify NACL

--- vpc/main.tf
+++ vpc/main.tf
@@ -28,51 +28,6 @@ resource "aws_network_acl" "dev" {
   subnet_ids = aws_subnet.dev[*].id
 
   ingress {
-    protocol   = "tcp"
-    rule_no    = 100
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 80
-    to_port    = 80
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 200
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-
-  ingress {
-    protocol   = "tcp"
-    rule_no    = 400
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-  ingress {
-    protocol   = "udp"
-    rule_no    = 500
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-  ingress {
-    protocol   = "icmp"
-    rule_no    = 600
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 0
-    to_port    = 0
-  }
-
-  ingress {
     protocol   = -1
     rule_no    = 1000
     action     = "allow"
