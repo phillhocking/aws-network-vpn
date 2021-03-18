25dc006a040d1eb1e3a769c9dbbb3b0275c3b7f4
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-03-18 05:47:06 -0700

    Added aws_default_route_table pointed at aws_internet_gateway.gw

--- vpc/main.tf
+++ vpc/main.tf
@@ -86,6 +86,20 @@ resource "aws_nat_gateway" "gw" {
 
 # Route Tables
 
+resource "aws_default_route_table" "main" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
+
+  route {
+    cidr_block    = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw
+  }
+
+  tags = {
+    Name = "default table"
+  }
+}
+
+
 resource "aws_route_table" "dev" {
   vpc_id = aws_vpc.main.id
 
