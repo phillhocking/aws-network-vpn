719dce97de5e4acb8bde1695acd24d3aaf4ac00e
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-04-08 20:11:31 -0700

    whaTEVER

--- main.tf
+++ main.tf
@@ -9,11 +9,9 @@ module "vpc" {
 
 module "vpn" {
   source                      = "./vpn"
-
-  aws_route_table_ids = [
-    module.vpc.route_table_ids[0],
-  ]
-
+  
+  aws_route_table_ids = module.vpc.route_table_ids
+  
   vpc_id                     = module.vpc.vpc_id
   prem_network_address_space = "10.20.0.0/16"
   prem_edge_ip               = var.edge_ip
