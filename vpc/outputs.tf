36984d586c2fe68588b37cdaca58f5cda1f87d6c
Phillip Hocking <Phillip.Hocking@excelsiorwellness.org>
2021-04-08 19:53:50 -0700

    remove square brackets

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -10,10 +10,10 @@ output "vpc_cidr_block" {
 
 output "route_table_ids" {
   value       = tolist(
-    [aws_route_table.prod.id],
-    [aws_route_table.staging.id],
-    [aws_route_table.dev.id], 
-    [aws_route_table.public.id]
+    [aws_route_table.prod.id,
+    aws_route_table.staging.id,
+    aws_route_table.dev.id, 
+    aws_route_table.public.id]
   )
 
   description = "Route tables for vpc subnets"
